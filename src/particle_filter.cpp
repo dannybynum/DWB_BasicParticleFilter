055c5d2762b57b2cfad6ec78307f75e4e524d84d
dannybynum <dannybynum@gmail.com>
2019-06-10 09:01:11 -0400

    additional updates during project

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -33,7 +33,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    *   (and others in this file).
    */
 
-  num_particles = 100;
+  num_particles = 5;
 
 vector<Particle> initial_particles;
 
@@ -105,9 +105,27 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
 		//changing heading based on heading rate and time
 		//also add in normally distributed random noise
 		//FIXME - see 21. Explanation of Code walk through - she describes noise differently.
-		particles[i].x += (velocity / yaw_rate)*((sin(particles[i].theta*yaw_rate*delta_t)) - sin(particles[i].theta)) + pred_noise_x(dwb_genit2);
-		particles[i].y += (velocity / yaw_rate)*(cos(particles[i].theta) - (cos(particles[i].theta*yaw_rate*delta_t))) + pred_noise_y(dwb_genit2);
+				
+		particles[i].x += (velocity / yaw_rate)*
+						  (sin(particles[i].theta + (yaw_rate*delta_t)) - sin(particles[i].theta)) + 
+						  pred_noise_x(dwb_genit2);
+		
+		particles[i].y += (velocity / yaw_rate)*
+						  (cos(particles[i].theta) - cos(particles[i].theta + (yaw_rate*delta_t))) +
+						  pred_noise_y(dwb_genit2);
+		
+		//leaving incorrect formula in place just for comparison purposes, but commenting out
+		//particles[i].y += (velocity / yaw_rate)*(cos(particles[i].theta) - (cos(particles[i].theta*yaw_rate*delta_t))) + pred_noise_y(dwb_genit2);
+		
 		particles[i].theta += (yaw_rate * delta_t) + pred_noise_theta(dwb_genit2);
+
+		
+		
+		
+		//debug to check how much pred_noise is getting added...may need to think about this some more
+		//particles[i].x += velocity*delta_t*cos(particles[i].theta) + pred_noise_x(dwb_genit2);
+		//particles[i].y += velocity * delta_t*sin(particles[i].theta) + pred_noise_y(dwb_genit2);
+		//particles[i].theta += velocity * delta_t + pred_noise_theta(dwb_genit2);
 
 		//dwb debug - print out to terminal to make sure setting it up properly
 		if (i < 2) {
@@ -127,6 +145,29 @@ void ParticleFilter::dataAssociation(vector<LandmarkObs> predicted,
 	 *   probably find it useful to implement this method and use it as a helper
 	 *   during the updateWeights phase.
 	 */
+
+	//LandmarkObs temp_measure_associateme;
+
+	//for (int k = 0; k < map.size(); ++k) {
+
+	//	// DWB - NOTE if using dataAssociation member-Function then - "Predicted" corresponds with the map
+	//	//                                                            "Observations" corresponds to particle
+	//	//
+	//	// FIXME - probably not how you access the map positions but 
+	//	//temp_dist = dist(temp_measure_associateme.x, temp_measure_associateme.y, map_landmarks[j].x, map_landmarks[j].y);
+
+	//	temp_measure_associateme.x = particles[i].sense_x[j];
+	//	temp_measure_associateme.y = particles[i].sense_y[j];
+
+	//	double temp_dist = 0;
+	//	double prev_temp_dist = 0;
+
+	//	if (temp_dist < prev_temp_dist) {
+
+	//		particles[i].associations[j] = map_landmarks[k].id;
+
+	//	}
+	//}
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -146,16 +187,22 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
 	
+
+	double accumulate_part_weights = 0;
+
 	//outer for loop is looping through each particle
 	for (int i = 0; i < num_particles; ++i) {
-		//inner for loop is looping through all the observations made for each time step, which then gets applied to each particle
+		
 
 		particles[i].sense_x.clear(); //using push_back to construct so have to clear it out each time - running this each time step in main
 		particles[i].sense_y.clear();
-		LandmarkObs temp_measure_associateme;
-
+		particles[i].associations.clear();
+		
+		LandmarkObs temp_measure_associateme; // make a temp struct to store transformed x,y particle point of view observations
+		
+		//inner for loop is looping through all the observations made for each time step, which then gets applied to each particle
 		for (int j = 0; j < observations.size(); ++j) {
-			
+
 			//computing the x,y observation back to map coordinates as applied/viewed from each particle.
 			//now each particle has a vector of x,y positions that correspond to map coordinates of where it would place landmarks according to the given observation (which was actually from the car)
 			//this will serve as a comparison to how good a particular particle represents the car - given the map and the observations -- since the actual observations are from the ACTUAL car - with some noise/uncertainty
@@ -166,37 +213,169 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 			temp_measure_associateme.y = particles[i].sense_y[j];
 
 			double temp_dist = 0;
-			double prev_temp_dist = 0;
+			double min_dist = 0;
+			//double prev_temp_dist = 0;
 
-			//FIXME - used 100 here, but this should be size of map - to increment through the full map to get the right association
-			for (int k = 0; k < 100; ++k) {
+			int best_id_index = 0; //the k that I want to bring outside the map landmark for loop
+			int best_id = 0; //the k that I want to bring outside the map landmark for loop
 
-				// DWB - NOTE if using dataAssociation member-Function then - "Predicted" corresponds with the map
-				//                                                            "Observations" corresponds to particle
-				//
-				// FIXME - probably not how you access the map positions but 
-				//temp_dist = dist(temp_measure_associateme.x, temp_measure_associateme.y, map_landmarks[j].x, map_landmarks[j].y);
+			// DWB FIXME - cycling through each map position for each measurement will probably be slow
+			// will likely have to implement a check upstream of this to determine map landmarks in sensor range of each particle
+			int debug_counter = 0;
 
-				if (temp_dist < prev_temp_dist) {
+			for (int k = 0; k < map_landmarks.landmark_list.size(); ++k) {
 
-					particles[i].associations[j] = map_landmarks[k].id;
+				// DWB - clearly don't really need the temp_measure_associateme, but maybe if passing to associate function
+				// DWB - this is using the helper function dist to pick the best landmark measurement
+				temp_dist = dist(temp_measure_associateme.x, temp_measure_associateme.y, map_landmarks.landmark_list[k].x_f, map_landmarks.landmark_list[k].y_f);
+				//std::cout << "temp_dist " << temp_dist << "prev_temp_dist "<<prev_temp_dist<<std::endl;
+			
+				if (k == 0) {
+					min_dist = temp_dist; //initialize min_dist to the first value;
+					//best_id_index = k;
+				}
+				
+				if (temp_dist < min_dist) {
+					min_dist = temp_dist;   //set a new min_dist
+					best_id_index = k;
+					
+					//debug_counter += 1;
+					//std::cout << "debug_counter " << debug_counter << std::endl;
+					//update the map association for this particular measurement as distance/fit to matp gets better 
+					//in the end only selecting one value for each 'j' here
+					
 
 				}
 
-				prev_temp_dist = temp_dist;
+				//setup previous distance for comparison purposes.
+				//prev_temp_dist = temp_dist;
+				
 
 			}
 
+			//build up the associations vector now that we've found best map landmark (id) fit
+			
+			best_id = map_landmarks.landmark_list[best_id_index].id_i;
+			
+			//std::cout << "best id " << best_id << std::endl;
+			particles[i].associations.push_back(best_id); 
 
-			//std::cout << particles[i].sense_x[1];
+		}
+		
+	
+		//now actually update the weights for each particle - FINALLY
+		//in order to update the weight I need to check "goodness" of each sensor measurement
+		//and then combine all of those goodness checks into the weight of the particle
+		//vector<double> particle_weight_elements;
+		//particle_weight_elements.clear();
 
-			//dwb debug - print out to terminal to make sure setting it up properly
-			if (i < 2) {
-				std::cout << "particle" << particles[i].id << " " << "size of new x vector" << particles[i].sense_x.size() << std::endl;
-			}
+		double measure_goodness_sum = 0;
+		double measure_goodness_product= 1;
+		
+
+		for (int ja = 0; ja<particles[i].associations.size(); ++ja){
+			//using code from the lesson - Lesson6.Module20: Particle Weights Solution
+			// calculate normalization term
+		
+			//using same names to input into the equation as was used in the lecture
+			//otherwise could have just used the values directly below in the calculations
+			double sig_x = std_landmark[0];  //brought into this function - given
+			double sig_y = std_landmark[1];  //brought into this function - given
+			double x_obs = particles[i].sense_x[ja];
+			double y_obs = particles[i].sense_y[ja];
+			//This one is a little deeper to follow 
+			//I want the "closest/associated" ACTUAL map x and y which I get by getting the ".x_f" from the landmark_list AT
+			//the id that is most closely associated with the particle measurement -- this is kept in particles.associations list
+			
+			double mu_x = map_landmarks.landmark_list[particles[i].associations[ja]-1].x_f;   //FIXME - not sure this is the best way but just noticed the index and id are only off by one
+																						      // would probably be better to actually "look up" the id I suppose - maybe with a loop
+			//double mu_x = map_landmarks.landmark_list[41].x_f;
+			double mu_y = map_landmarks.landmark_list[particles[i].associations[ja]-1].y_f;
+
+			//std::cout << "ja "<<ja<< "associations " << particles[i].associations[ja]<<std::endl;
+		
+			double term1_gauss_norm;
+			term1_gauss_norm = 1 / (2 * M_PI * sig_x * sig_y);
+
+			// calculate exponent
+			double term2_exponent;
+			term2_exponent = (pow(x_obs - mu_x, 2) / (2 * pow(sig_x, 2)))
+				+ (pow(y_obs - mu_y, 2) / (2 * pow(sig_y, 2)));
+
+			// calculate weight using normalization terms and exponent
+			//vector<double> particle_weight_elements
+			
+			
+			measure_goodness_sum += (term1_gauss_norm * exp(-term2_exponent));
+			measure_goodness_product *= (term1_gauss_norm * exp(-term2_exponent));
+			//particle_weight_elements.push_back(term1_gauss_norm * exp(-term2_exponent));
+
+			//LEFT OFF HERE on JUNE-4 - PICK UP HERE - not sure why sometimes more than 3 print out in here....hmmmmmm
+			//also is the exp of a large negative number close to 0 - is that why this is zero - if so maybe ok? or look at that term2_exponent some more
+			//if (ja < 1) {
+				//std::cout << "term1 " << term1_gauss_norm <<"term2  " << term2_exponent << std::endl;
+				//std::cout << "mu_x " << mu_x << " x_obs " << x_obs << std::endl;
+			//}
+
+			//FIXME - this might work but do I really want to use the old particle weight each delta_time step?
+			//maybe but not sure this is how it worked in the lesson - maybe just wipe out the old weight and the
+			//new weight becomes the multipled together multi-var-gaussian
+			
+			//here is where I left off - need to figure out the best way to calculate the weight of each particle ... seems to go to zero...
+			//changed weights to try to normalize it but the weights still evaluate to zero (very small number) after the first particle set..
+			//I could move on for now and try to implement the code that picks the best particles and see if that makes things work better
+			// even though I don't like or understand why they start evaluating to zero - maybe ignore that and try to make some progress
+			// without fully understanding that at the moment
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+			//particles[i].weight *= weight;
+
+
+
 		}
 
+		particles[i].weight = measure_goodness_product / measure_goodness_sum; //normalize so that result is between 0 and 1
+
+		accumulate_part_weights += particles[i].weight;
+
+		//dwb debug - print out to terminal to make sure setting it up properly
+		//this is printing for each particle so it is in the outer for loop here
+		if (i < 2) {
+		std::cout << "particle " << particles[i].id << " " << "size_of_x_vect " << particles[i].sense_x.size()<< 
+			" size_of_associations_vect " << particles[i].associations.size()<< std::endl;
+		}
+
+		if (i < 2) {
+			std::cout << "particle " << particles[i].id << " " << " weight " << particles[i].weight << std::endl;
+		}
+
+
+	
 	}
+
+	//python code for reference
+	//def multivar_gauss(x, y, mu_x, mu_y, sdev_x, sdev_y) :
+	//	term1 = 1 / (2 * math.pi*sdev_x*sdev_y)
+	//	term2a = ((x - mu_x)**2) / (2 * (sdev_x**2))
+	//	term2b = ((y - mu_y)**2) / (2 * (sdev_y**2))
+	//	term2 = -1 * (term2a + term2b)
+
+	//	return term1 * math.exp(term2)
+
+	//loop through particles one more time to do normalization
+	for (int m = 0; m < num_particles; ++m) {
+		particles[m].weight = particles[m].weight/accumulate_part_weights; //normalize so that between 0 and 1
+
+		if (m < 5) {
+			std::cout << "particle " << particles[m].id << " " << "normd weight" << particles[m].weight << std::endl;
+		}
+		
+	}
+
 }
 
 void ParticleFilter::resample() {
@@ -206,6 +385,89 @@ void ParticleFilter::resample() {
    * NOTE: You may find std::discrete_distribution helpful here.
    *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
    */
+	std::vector<Particle> resampled_particles;
+	
+	double temp_find_max_weight = 0;
+	double accumulate_part_weights = 0;
+
+	//loop through particles to find max weight
+	for (int i = 0; i < num_particles; ++i) {
+		if (particles[i].weight > temp_find_max_weight){
+			temp_find_max_weight = particles[i].weight;
+		}
+
+	}
+
+	std::cout << "temp_find_max_weight " << temp_find_max_weight << std::endl;
+
+	//clear out the resampled_particles vector between runs
+	resampled_particles.clear();
+
+	//loop through particles to do resampling
+	for (int i = 0; i < num_particles; ++i) {
+
+
+		//dwb using random engine to get random value within distribution -this was used in lesson materials
+		std::default_random_engine dwb_genit3;
+		std::default_random_engine dwb_genit4;
+
+		double curr_sample_wheel_beta = 0;
+		int sample_index = 0;
+
+		//using uniform int distribution to chose a random index
+		std::uniform_int_distribution<int> random_part_index_dist(0,num_particles-1);
+		
+		// using uniform distribution - following python implmentation from lesson5
+		std::uniform_real_distribution<double> random_samp_whl_beta(0, 2*temp_find_max_weight);
+
+		sample_index = random_part_index_dist(dwb_genit3);
+		curr_sample_wheel_beta = random_samp_whl_beta(dwb_genit4);
+
+		if (i < 5) {
+			std::cout << "sample_index " << sample_index << " " << "beta " << curr_sample_wheel_beta << std::endl;
+		}
+
+		// this while loop will pass through to using the current sample index to select the particle if beta is smaller than the current weight
+		// this implementing the sampling wheel idea where the arc-length of each element in the wheel is equal to the particle weight
+		
+		while (curr_sample_wheel_beta > particles[sample_index].weight) {
+			curr_sample_wheel_beta -= particles[sample_index].weight; // remove the amount of beta that has been used up moving to the next index spot
+
+			sample_index = (sample_index + 1) % (num_particles); //same trick as in Python - using modulo so that index wraps around
+
+			//std::cout << "sample wheel index " << sample_index;
+		}
+
+		//sample_index = 0;
+
+		resampled_particles.push_back(particles[sample_index]); //build up the resampled_particles vector
+
+/*     Python implementation of resampling wheel from course (lesson 5)
+		p3 = []
+			index = int(random.random()*N)   #random 0 - 1 times 1000 gives random index
+			print("index ", index)
+			beta = 0.0
+			mw = max(w)
+
+			for i in range(N) :
+				beta += random.random()*2.0*mw
+				#print("beta_outer ", beta, "/n")
+				#if beta is less than weight of index then pick that index/particle again
+				while beta > w[index]:
+					#print("beta_inner ", beta, "/n")
+					#keep subtracting the current weight from beta  and incrementing the index - interesting ?
+					beta -= w[index]
+					index = (index + 1) % N  #interesting so there is a random starting place but the modulo helps it "wrap around"
+					# to cover all of the values.
+				p3.append(p[index])
+			
+			p = p3
+			print p #please leave this print statement here for grading!*/
+
+	
+	}
+	
+	particles = resampled_particles; //set the main particle set to the newly resampled particle set
 
 }
 
